a912c53d9c149efac3113478e6f5e16caef25e40
Renuka Patil <renukapatil627@gmail.com>
2026-03-19 18:02:50 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
 
 function App() {
   return (
     <div className="App">
+      <HookCounter />
+      {/* <ClassCounter /> */}
+
       {/* <PostList /> */}
-      <PostForm />
+      {/* <PostForm /> */}
     </div>
   );
 }
