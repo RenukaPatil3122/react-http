2c3c93d7caad1c9731bbf568616eec9d42853be6
Renuka Patil <renukapatil627@gmail.com>
2026-03-20 15:54:44 +0530

    conditionally run effects

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import HookCounter from "./components/HookCounter";
 import HookCounterTwo from "./components/HookCounterTwo";
 import HookCounterThree from "./components/HookCounterThree";
 import HookCounterFour from "./components/HookCounterFour";
+import HookCounterOne from "./components/HookCounterOne";
+import ClassCounterOne from "./components/ClassCounterOne";
 
 function App() {
   return (
     <div className="App">
-      <HookCounterFour />
+      {/* <ClassCounterOne /> */}
+      <HookCounterOne />
+
+      {/* <HookCounterFour /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounter /> */}
